36de9e7e939d560a997dac925180c99c19c7467a
Taekmin Kim <tantara.tm@gmail.com>
2013-03-26 13:59:21 +0900

    simple modeling

---
+++ Broadcast/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Broadcast.models import Game
+from Broadcast.models import GameLog
+
+admin.site.register(Game)
+admin.site.register(GameLog)
---
+++ Broadcast/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Setting(models.Model): # Voice, etc
+	## Field
+	name = models.CharField(max_length=140)
+
+class Game(models.Model):
+	## Relation
+	setting = models.ForeignKey(Setting)
+
+	## Custom Field
+	url = models.CharField(max_length=200)
+	title = models.CharField(max_length=140)
+
+	## Naver Field
+	game_id = models.CharField("Naver gameId", max_length=200)
+	date = models.DateTimeField("Naver gdate")
+	day = models.CharField("Naver gweek", max_length=1)
+	a_full_name = models.CharField("Naver aFullName", max_length=200)
+	h_full_name = models.CharField("Naver hFullName", max_length=200)
+	a_score = models.IntegerField("Naver aScore", default=0)
+	h_score = models.IntegerField("Naver hScore", default=0)
+	rheb = models.CommaSeparatedIntegerField("Naver rheb")
+	score_board = models.CommaSeparatedIntegerField("Naver scoreBoard")
+
+class GameLog(models.Model):
+	## Relation
+	game = models.ForeignKey(Game)
+
+	## Naver Field
+	inn = models.IntegerField(default=1)
+	seqno = models.IntegerField(default=1)
+	live_text = models.CharField(max_length=200)
+	text_style = models.IntegerField(default=0)
+	btop = models.IntegerField(default=0)
