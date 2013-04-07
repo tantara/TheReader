55e04323aa51bf3f9bbe6640ac7a7acdf2e4f6f3
Taekmin Kim <tantara.tm@gmail.com>
2013-04-07 21:42:12 +0900

    can import local libraries

--- manage.py
+++ manage.py
@@ -3,8 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TheReader.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TheReader.settings")
 
-    from django.core.management import execute_from_command_line
+	from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	project_root = os.path.dirname(os.path.realpath(__file__))
+	sys.path.append(os.path.join(project_root, 'utils'))
+
+	execute_from_command_line(sys.argv)
