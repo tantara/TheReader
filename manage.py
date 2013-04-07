418488d505a0db01dfc42a7aa6d949fc687e98c3
Taekmin Kim <tantara.tm@gmail.com>
2013-04-07 21:44:00 +0900

    revert manager.py

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import os
-import sys
 
 if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TheReader.settings")
 
 	from django.core.management import execute_from_command_line
-
-	project_root = os.path.dirname(os.path.realpath(__file__))
-	sys.path.append(os.path.join(project_root, 'utils'))
 
 	execute_from_command_line(sys.argv)
