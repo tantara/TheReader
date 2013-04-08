657450f288288e1feb3bfc60a6c738293990d658
msgundam00 <gundamdouble00@gmail.com>
2013-04-08 15:25:25 +0900

    Update settings.py
    tab 간격 조정.. 흠.?!

--- TheReader/settings.py
+++ TheReader/settings.py
@@ -21,7 +21,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': os.path.join(SITE_ROOT, 'db') + '/sqlite3.db', # Or path to database file if using sqlite3.
+        'NAME': os.path.join(SITE_ROOT, 'db') + '/sqlite3.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -134,8 +134,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-	'Broadcast',
-	'nsd_utils',
+    'Broadcast',
+    'nsd_utils',
 )
 
 # A sample logging configuration. The only tangible logging
