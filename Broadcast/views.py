da2a0862d948206f1609239c5e373281bf341f95
DongShik Hahm <gundamdouble00@gmail.com>
2013-04-08 22:12:18 +0900

    change nsdUtils name

--- Broadcast/views.py
+++ Broadcast/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
-import nsd_utils.nsd_parser
+from nsdUtils.nsdParser import UrlParser
 
 def show(request):
 	if request.method == "POST":
 		form = request.POST
 		url = form['url']
+		nsd_data = UrlParser(url)
 		return render_to_response('broadcast/show.html', {'url': url})
 	else:
 		return HttpResponse(status=500)
--- TheReader/settings.py
+++ TheReader/settings.py
@@ -7,6 +7,7 @@ import django
 
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+print SITE_ROOT
 # END - For relative path
 
 DEBUG = False
