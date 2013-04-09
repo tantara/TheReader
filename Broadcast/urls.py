bc4d17b4a4c4b84b583a542868953f5dd6b17a57
Taekmin Kim <tantara.tm@gmail.com>
2013-04-09 10:34:23 +0900

    changed directory hierachy(static, db, templates)

--- Broadcast/urls.py
+++ Broadcast/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'Broadcast.views.show', name='show')
+    url(r'^$', 'Broadcast.views.show', name='show'),
+    url(r'^reload$', 'Broadcast.views.reload', name='reload')
 )
--- TheReader/settings.py
+++ TheReader/settings.py
@@ -21,7 +21,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'db') + '/sqlite3.db', # Or path to database file if using sqlite3.
+        'NAME': os.path.join(SITE_ROOT, '../db') + '/sqlite3.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -69,7 +69,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_ROOT = os.path.join(SITE_ROOT, '../static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -80,7 +80,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'static')
+    os.path.join(SITE_ROOT, '../static')
 )
 
 # List of finder classes that know how to find static files in
@@ -120,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'templates')
+    os.path.join(SITE_ROOT, '../templates')
 )
 
 INSTALLED_APPS = (
