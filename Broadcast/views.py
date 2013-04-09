cfc834b7dd826586764d191750fea638261123e3
Taekmin Kim <tantara.tm@gmail.com>
2013-04-09 23:24:35 +0900

    broadcast reload

--- Broadcast/views.py
+++ Broadcast/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 from nsdUtils.nsdParser import UrlParser
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 def show(request):
 	if request.method == "POST":
-		form = request.POST
-		url = form['url']
+		url = request.POST['url']
+		return render_to_response('broadcast/show.html', {'url': url})
+	else:
+		return HttpResponse(status=500)
+
+@csrf_exempt
+def reload(request):
+	if request.method == "POST":
+		url = request.POST['url']
 		nsdData = UrlParser(url)
 		nsdData.parsingLiveTexts()
-		return render_to_response('broadcast/show.html', {'url': url, 'nsdData': nsdData})
+		return HttpResponse(json.dumps(nsdData.liveTexts))
 	else:
 		return HttpResponse(status=500)
