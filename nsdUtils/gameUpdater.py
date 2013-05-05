555bd778bd859c05505ab539ece280a8b1336133
DongShik Hahm <gundamdouble00@gmail.com>
2013-05-05 13:59:40 +0900

    라이브 텍스트 flag파싱

--- nsdUtils/gameUpdater.py
+++ nsdUtils/gameUpdater.py
@@ -4,6 +4,7 @@ import urllib
 import ast
 import operator
 import time
+import re
 from Broadcast.models import Game,GameLog
 
 class GameUpdater: # Gmae db Updater
@@ -53,6 +54,7 @@ class GameUpdater: # Gmae db Updater
 				newLog.seqno = text['seqno']
 				newLog.live_text = text['liveText']
 				newLog.btop = text['btop']
+				newLog.flag = self.parseFlag(text['liveText'])
 				newLog.save()
 
 				curSeqno = text['seqno']
@@ -75,3 +77,75 @@ class GameUpdater: # Gmae db Updater
 			return False
 
 		return GameLog.objects.filter(game=game, seqno=seqNo)[0].live_text != u'경기종료'
+
+	def parseFlag(self, liveText):
+		if self.hitFlag:
+			if bool(re.search(u'\d번타자', liveText)):
+				self.hitFlag = False;
+				return 310 + int(re.findall('\d', liveText)[0])
+			elif bool(re.search(u'\d루주자', liveText)):
+				position = int(re.findall('\d', liveText)[0])
+				if u'진루' in livetext:
+					return 21100 + position * 10 + int(re.findall('\d', liveText)[1])
+				elif u'홈인' in liveText:
+					return 21104 + position * 10
+				elif u'태그아웃' in liveText:
+					return 2230 + position
+			elif u'안타' in liveText:
+				return 121
+			elif u'2루타' in liveText:
+				return 122
+			elif u'3루타' in liveText:
+				return 123
+			elif u'홈런' in liveText:
+				return 124
+			elif u'희생번트' in liveText:
+				return 128
+			elif u'실책으로 출루' in liveText:
+				return 129
+			elif u'플라이' in liveText:
+				return 132
+			elif u'땅볼' in liveText:
+				return 133
+			elif u'병살타' in liveText:
+				return 134
+##			elif u'삼중살' in liveText: 아직 모름.
+##				return 135
+			elif u'희생번트' in liveText:
+				return 136
+			else:
+				return 0
+		else:
+			if u'타격' in liveText:
+				self.hitFlag = True;
+				return 0
+			elif bool(re.search(u'\d번타자', liveText)):
+				return 310 + int(re.findall('\d', liveText)[0])
+			elif bool(re.search(u'\d루주자', liveText)):
+				position = int(re.findall('\d', liveText)[0])
+				if u'도루실패' in liveText:
+					return 2220 + position
+				elif u'도루' in liveText:
+					return 2130 + position
+				elif u'진루' in livetext:
+					return 21100 + position * 10 + int(re.findall('\d', liveText)[1])
+				elif u'홈인' in liveText:
+					return 21104 + position * 10
+			elif u'볼넷' in liveText:
+				return 125
+			elif u'몸에 맞는 볼' in liveText:
+				return 126
+			elif u'스트라이크 낫아웃' in liveText:
+				return 127
+			elif u'스트라이크' in liveText:
+				return 111
+			elif u'볼' in liveText:
+				return 112
+			elif u'파울' in liveText:
+				return 113
+			elif u'헛스윙' in liveText:
+				return 114
+			elif u'삼진 아웃' in liveText:
+				return 131
+			else:
+				return 0;
